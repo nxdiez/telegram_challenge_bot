501c6588b308df8957d642f9e6823a124e9ff314
NxDiez <nxdiez@users.noreply.github.com>
2019-08-25 18:13:33 +0200

    Create keys.js

---
+++ keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    telegramSetup: {
+        TELEGRAM_BOT_TOKEN:'',//put here your BOT Token
+        CANAL:'-0000',//put here the channel
+        GRUPOFOTOS:'-11111' // Put here validator channel
+    },
+    database:{
+        file: 'xxx.db' //Put here path to DB
+    }
+       
+}
